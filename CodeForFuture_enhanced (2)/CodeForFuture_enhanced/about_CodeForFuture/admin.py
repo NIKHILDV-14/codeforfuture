d5348e3de37f2620e0178e6e0f6c3b49eb427f0b
NIKHILDV-14 <nikhildv69@gmail.com>
2026-05-17 16:05:29 +0530

    Delete CodeForFuture_enhanced (2)/CodeForFuture_enhanced directory
    my life my rulezzz

--- CodeForFuture_enhanced (2)/CodeForFuture_enhanced/about_CodeForFuture/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Internship, Hackathon, FreeCourse, ContactMessage
-
-admin.site.register(Internship)
-admin.site.register(Hackathon)
-admin.site.register(FreeCourse)
-admin.site.register(ContactMessage)
--- CodeForFuture_enhanced (2)/CodeForFuture_enhanced/about_CodeForFuture/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AboutCodeforfutureConfig(AppConfig):
-    name = 'about_CodeForFuture'
